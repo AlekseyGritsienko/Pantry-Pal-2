a896da76cfa7483c5debe4b6af7c4ad20f3915c0
Aleksey Gritsienko <gritsienko@kstu.kg>
2026-05-05 15:51:48 +0600

    inventory endpoint added

--- cmd/main.go
+++ cmd/main.go
@@ -8,19 +8,24 @@ import (
 )
 
 func main() {
-	// создаём роутер
 	r := chi.NewRouter()
 
-	// endpoint /health
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 	})
 
-	log.Println("Server started on :8080")
+	r.Get("/inventory", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := `[{"name":"Milk","quantity":1}]`
+		w.Write([]byte(response))
+	})
 
-	// запускаем сервер
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		log.Fatal(err)
-	}
+	r.Post("/inventory", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		response := `{"message":"item created"}`
+		w.Write([]byte(response))
+	})
+
+	log.Println("Server started on :8080")
+	http.ListenAndServe(":8080", r)
 }
